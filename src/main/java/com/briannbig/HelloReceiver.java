afd8e694c6d7fd13829820447f7f8e1265ddbdbd
brian-big <briannbig@gmail.com>
2022-10-17 18:54:03 +0300

    minor fix

--- src/main/java/com/briannbig/HelloReceiver.java
+++ src/main/java/com/briannbig/HelloReceiver.java
@@ -15,6 +15,8 @@ public class HelloReceiver {
                 processor.doProcess(receivedMessage);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            } finally {
+                System.out.println(" [ x ] DONE");
             }
         });
         Config.getInstance().getDefaultChannel().basicConsume(Config.QUEUE_NAME, true, deliverCallback, consumerTag -> {} );
