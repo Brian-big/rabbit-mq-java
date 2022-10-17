f06b7c0998c195580df76dfc91a7f4035a7fe7aa
brian-big <briannbig@gmail.com>
2022-10-17 19:15:20 +0300

    work queues: Message Persistence

--- src/main/java/com/briannbig/Send.java
+++ src/main/java/com/briannbig/Send.java
@@ -1,12 +1,15 @@
 package com.briannbig;
 
+import com.rabbitmq.client.MessageProperties;
+
 import java.io.IOException;
 
 public class Send {
     public void send(String message) throws IOException {
         String msg = String.join(" ", message);
         Config.getInstance().getDefaultChannel().queueDeclare(Config.DURABLE_QUEUE, true, false, false, null);
-        Config.getInstance().getDefaultChannel().basicPublish("", Config.QUEUE_NAME, null, msg.getBytes());
+        Config.getInstance().getDefaultChannel().basicPublish("", Config.QUEUE_NAME,
+                MessageProperties.PERSISTENT_TEXT_PLAIN, msg.getBytes());
         System.out.println(" [x] Sent '" + msg +"'");
     }
 }
